de3fbfb133032c74815a4728061a0dd3b09858c9
Hamza <hamza.abdulilah@smartrefill.se>
2023-03-01 13:10:43 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RefillSDKFramework
+
+A description of this package.
---
+++ Sources/RefillSDKFramework/RefillSDKFramework.swift
@@ -0,0 +1,6 @@
+public struct RefillSDKFramework {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
